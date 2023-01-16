07d0d05eaf20e0cd28d757f31ae79376a5c33458
Egor <you@example.com>
2023-01-16 17:53:59 +0300

    введены задержки для генерации уникальных timestamp

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -5,38 +5,39 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Generator {
 
-    private static final String dataFileName = "files/random-file";
+    private static final String dataFileName = "random-file";
     private static long writtenBytes;
-    private static void generateFileWithOpenTSDBData(byte number, long fileSize) throws IOException {
+    private static void generateFileWithOpenTSDBData(byte number, long fileSize) throws IOException, InterruptedException {
 
-            String fileName = String.format("%s%s", dataFileName,number);
-            do{
-                int cpuTagValue = ThreadLocalRandom.current().nextInt(1, 32);
-                long unixSecondsNow = Instant.now().toEpochMilli()-cpuTagValue-1;
-                if(cpuTagValue%2==0) unixSecondsNow = Instant.now().toEpochMilli()+cpuTagValue+1;
-                int hostTagValue = ThreadLocalRandom.current().nextInt(100, 200) + 1;
-                try (FileWriter fileWriter = new FileWriter(fileName,true);
-                     BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-                     PrintWriter printWriter = new PrintWriter(bufferedWriter)) {
-                    String tsdbLine = String.format("sys.cpu.user %d %d host=web0%d cpu=%d",
-                            unixSecondsNow,hostTagValue,number,cpuTagValue);//add tagv to OpenTSDB
-                    printWriter.print(tsdbLine);
-                    printWriter.print(System.lineSeparator());
-                    writtenBytes += tsdbLine.getBytes(StandardCharsets.UTF_8).length;
-                    writtenBytes += System.lineSeparator().getBytes(StandardCharsets.UTF_8).length;
-                }
-                System.out.format("bytes written: %d\n",writtenBytes);
+        String fileName = String.format("%s%s", dataFileName,number);
+        do{
+            Thread.sleep(10);
+            int cpuTagValue = ThreadLocalRandom.current().nextInt(1, 32);
+            long unixSecondsNow = Instant.now().toEpochMilli();//-cpuTagValue-1;
+            //if(cpuTagValue%2==0) unixSecondsNow = Instant.now().toEpochMilli();//+cpuTagValue+1;
+            int hostTagValue = ThreadLocalRandom.current().nextInt(100, 200) + 1;
+            try (FileWriter fileWriter = new FileWriter(fileName,true);
+                 BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                 PrintWriter printWriter = new PrintWriter(bufferedWriter)) {
+                String tsdbLine = String.format("sys.cpu.user %d %d host=web0%d cpu=%d",
+                        unixSecondsNow,hostTagValue,number,cpuTagValue);//add tagv to OpenTSDB
+                printWriter.print(tsdbLine);
+                printWriter.print(System.lineSeparator());
+                writtenBytes += tsdbLine.getBytes(StandardCharsets.UTF_8).length;
+                writtenBytes += System.lineSeparator().getBytes(StandardCharsets.UTF_8).length;
             }
-            while (writtenBytes < fileSize);
+            System.out.format("bytes written: %d\n",writtenBytes);
+        }
+        while (writtenBytes < fileSize);
     }
 
     public static void main(String[] args) {
         try {
-                int randomSize = ThreadLocalRandom.current().nextInt(100 * (1024 * 1024), 200 * (1024 * 1024));
-                System.out.format("File generation with size %d MB has started.", randomSize / (1024 * 1024));
-                generateFileWithOpenTSDBData((byte) 9, randomSize);
+            int randomSize = ThreadLocalRandom.current().nextInt(100 * (1024 * 1024), 200 * (1024 * 1024));
+            System.out.format("File generation with size %d MB has started.", randomSize / (1024 * 1024));
+            generateFileWithOpenTSDBData((byte) 9, randomSize);
 
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             System.out.println(e.getMessage());
             throw new RuntimeException(e);
         }
@@ -44,3 +45,4 @@ public class Generator {
     }
 
 }
+
