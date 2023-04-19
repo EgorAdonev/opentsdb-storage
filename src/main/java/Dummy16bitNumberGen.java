4b60b906f17cc47c0a416d04e02b521b6b66b09e
Egor <you@example.com>
2023-04-19 18:23:23 +0300

    fix

--- src/main/java/Dummy16bitNumberGen.java
+++ src/main/java/Dummy16bitNumberGen.java
@@ -1,12 +1,6 @@
 public class Dummy16bitNumberGen {
     public static void main(String[] args) {
-        String dayOfWeek = null;
-        switch(dayOfWeek) {
-            case "MON":
-                System.out.println(1);
-        }
-    }
-    
 
+    }
 
 }
