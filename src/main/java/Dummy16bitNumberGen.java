eb07451e5b622b76c541a53b2957f23da5c37fdf
Egor <you@example.com>
2023-04-26 17:25:48 +0300

    fixed number converter

--- src/main/java/Dummy16bitNumberGen.java
+++
@@ -1,6 +0,0 @@
-public class Dummy16bitNumberGen {
-    public static void main(String[] args) {
-
-    }
-
-}
--- src/main/java/NumberConverter.java
+++ src/main/java/NumberConverter.java
@@ -21,19 +21,29 @@ public class NumberConverter {
 //            System.out.println(readCsvFileFromHardware("C:\\Users\\egodo\\Downloads\\experiment_rez\\Test2.ch2.csv"));
     }
 
-    public static List<Double> readCsvFileFromHardware(File file) throws IOException {
+    public static List<Float> readCsvFileFromHardware(File file) throws IOException {
         String line;
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            List<List<String>> list = new ArrayList<>();
-            List<Double> doubleList = new ArrayList<>();
+            List<Float> doubleList = new ArrayList<>();
             while ((line = br.readLine()) != null) {
-                list.add(Arrays.asList(line.split(";")));
-                for (List<String> stringList: list) {
-                    for (String str : stringList) {
-                        double number = Short.parseShort(str)/Math.pow(10,(double) str.toCharArray().length);
+                    for (String str : line.split(";")) {
+                        short num = Short.parseShort(str);
+                                //str.charAt(0) == '-' ? (short) (Short.parseShort(str) * (-1)) : Short.parseShort(str);
+                        //num = (short) (0b1 << num);
+                        //побитовое отрицание
+                        String twoInPower = Integer.toBinaryString((int)Math.pow(2.0,15.0)).replace('0','1');
+                        //char[] arr = twoInPower.toCharArray();
+                        num &= Integer.parseInt(twoInPower,2)>>1;
+                        String bin = Integer.toBinaryString(num);
+                        short sh = bin.charAt(0) == '-' ? (short) (Short.parseShort(str) * (-1)) : Short.parseShort(str);
+                        //System.out.println(bin.length());
+                        bin = String.valueOf(sh);
+                        //bin = String.valueOf(Short.parseShort(bin,2));
+                        //int i = bin.charAt(0) == '1' ? Integer.parseInt(str) << 0b0 : Integer.parseInt(str) << 0b1;
+                        float number = (float) Float.parseFloat(bin);
+                               // /Math.pow(10,(double) str.toCharArray().length);
                         doubleList.add(number);
                     }
-                }
             }
             return doubleList;
         }
